51a27ece11332b7cb9cedc61c93ffb4c0c14173c
GuilkMarques <guilhermeluciano99@gmail.com>
2023-12-04 10:23:01 -0300

    Update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule, LOCALE_ID } from "@angular/core";
 import {
     BrowserModule,
     provideClientHydration,
@@ -32,6 +32,11 @@ import { ProductRead2Component } from "./components/products/product-read2/produ
 import { MatTableModule } from "@angular/material/table";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSortModule } from "@angular/material/sort";
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from "@angular/common";
+import { ProductUpdateComponent } from './components/products/product-update/product-update.component';
+
+registerLocaleData(localePt)
 
 @NgModule({
     declarations: [
@@ -47,6 +52,7 @@ import { MatSortModule } from "@angular/material/sort";
         ProductCreateComponent,
         ProductReadComponent,
         ProductRead2Component,
+        ProductUpdateComponent,
     ],
     imports: [
         BrowserModule,
@@ -66,7 +72,9 @@ import { MatSortModule } from "@angular/material/sort";
         MatPaginatorModule,
         MatSortModule,
     ],
-    providers: [provideClientHydration()],
+    providers: [provideClientHydration(),
+    {provide: LOCALE_ID,
+    useValue: 'pt-BR'}],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
--- frontend/src/app/components/products/product.service.ts
+++ frontend/src/app/components/products/product.service.ts
@@ -1,15 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { Product } from "./product.model"
+import { Product } from "./product.model";
 import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: "root",
 })
 export class ProductService {
-
-    baseUrl = "http://localhost:3000/products"
+    baseUrl = "http://localhost:3000/products";
 
     constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
@@ -21,11 +20,26 @@ export class ProductService {
         });
     }
 
-    create(product: Product): Observable<Product>{
-      return this.http.post<Product>(this.baseUrl, product)
+    create(product: Product): Observable<Product> {
+        return this.http.post<Product>(this.baseUrl, product);
     }
 
-    read(): Observable<Product[]>{
-        return this.http.get<Product[]>(this.baseUrl)
+    read(): Observable<Product[]> {
+        return this.http.get<Product[]>(this.baseUrl);
     }
+
+    readByID(id: string): Observable<Product> {
+        const url = `${this.baseUrl}/${id}`;
+        return this.http.get<Product>(url);
+    }
+
+    update(product: Product): Observable<Product> {
+        const url = `${this.baseUrl}/${product.id}`;
+        return this.http.put<Product>(url, product);
+    }
+
+    // delete(product: Product): Observable<Product>{
+    //     const url = `${this.baseUrl}/${product.id}`
+    //     return this.http.delete<Product>(url)
+    // }
 }
